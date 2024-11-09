626f0d51cf9948d0b90e9784fa139a8c3cee4eea
Raunak Raj <71929976+bajrangCoder@users.noreply.github.com>
2024-11-09 22:43:54 +0530

    feat: added option to provide custom lsp path, arg

--- readme.md
+++ readme.md
@@ -10,9 +10,26 @@ Mojo language support for Zed. It provides syntax highlighting and lsp.
 
 Provide your feedback to improve this extension as I don't use mojo language.
 
+## Settings
+
+You can configure the lsp by changing the zed settings
+
+```json
+{
+  "lsp": {
+    "mojo": {
+      "settings": {
+        "lsp_path": "/name/my-proj/.magic/envs/default/bin/mojo-lsp-server", // path to mojo-lsp-server
+        "args": ["--log=verbose"] // additional args for mojo-lsp-server
+      }
+    }
+  },
+}
+```
+
 ## Installation
 
 - Clone this repository.
 - From zed extension page click "Install Dev Extension" and select the cloned repository.
 
-> Note: This extension is not published to zed extension store.
+> Note: This extension is not published to zed extension store. As this isn't stable and it isn't complete.
--- src/mojo.rs
+++ src/mojo.rs
@@ -1,28 +1,53 @@
-use zed_extension_api::{self as zed, Result};
+use zed_extension_api::{self as zed, settings::LspSettings, Result};
 
 struct MojoExtension {}
 
-
 impl zed::Extension for MojoExtension {
     fn new() -> Self {
-         Self {}
-     }
+        Self {}
+    }
 
-     fn language_server_command(
-         &mut self,
-         _: &zed::LanguageServerId,
-         worktree: &zed::Worktree,
-     ) -> Result<zed::Command> {
-         let path = worktree
-         .which("mojo-lsp-server")
-         .ok_or_else(|| "Must install `mojo-lsp-server` [via Modular](https://developer.modular.com/download), its comes with mojo package".to_string())?;
+    fn language_server_command(
+        &mut self,
+        language_server_id: &zed::LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<zed::Command> {
+        let settings = LspSettings::for_worktree(language_server_id.as_ref(), worktree)?;
 
-         Ok(zed::Command {
-             command: path,
-             args: vec![],
-             env: Default::default(),
-         })
-     }
+        let error_message = "Must install `mojo-lsp-server` [via Modular](https://developer.modular.com/download), its comes with mojo package";
+
+        let settings_clone = settings.settings.clone();
+        let mojo_binary_path = settings_clone
+            .and_then(|s| {
+                s.get("lsp_path")
+                    .and_then(|v| v.as_str())
+                    .map(|s| s.to_string())
+            })
+            .unwrap_or_else(|| {
+                worktree
+                    .which("mojo-lsp-server")
+                    .ok_or_else(|| error_message.to_string())
+                    .unwrap()
+            });
+
+        let args = settings
+            .settings
+            .and_then(|s| s.get("args").cloned())
+            .and_then(|v| {
+                v.as_array().map(|arr| {
+                    arr.iter()
+                        .filter_map(|v| v.as_str().map(String::from))
+                        .collect()
+                })
+            })
+            .unwrap_or_default();
+
+        Ok(zed::Command {
+            command: mojo_binary_path.to_string(),
+            args,
+            env: Default::default(),
+        })
+    }
 }
 
 zed::register_extension!(MojoExtension);
